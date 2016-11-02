ec88ded083e06e9b456cfe26459224e195d9ac91
eter <eter365@gmail.com>
2016-11-02 15:04:11 +0800

    add init mongodb2.6

--- playbooks/roles/replset/templates/adduser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-############
-# ADD USER #
-############
-HOST="$1"
-PORT="$2"
-MONGO_BIN="/opt/soft/mongodb3210/bin/mongo"
-SCRIPTPATH="/work/github/ansible/playbooks/roles/replset/templates"
-${MONGO_BIN} ${HOST}:${PORT}/admin --authenticationDatabase admin ${SCRIPTPATH}/adduser.js
